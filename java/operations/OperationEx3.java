97f447edb2e33c0c473ffc6e19319f053a1ba9e0
tunachoi-dev <227953280+tunachoi-dev@users.noreply.github.com>
2026-03-20 00:44:39 +0900

    docs: operation exercise

---
+++ java/operations/OperationEx3.java
@@ -0,0 +1,9 @@
+package operation;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 15;
+        boolean result = score >= 80 && score <= 100;
+        System.out.println(result);
+    }
+}
